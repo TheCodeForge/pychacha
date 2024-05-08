252d2a1fbd1c88aee7ee2ac3dcde1f37f64a4596
TheCodeForge <92867553+TheCodeForge@users.noreply.github.com>
2024-05-08 15:55:02 -0400

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="pychacha",
-    version="1.0.2",
+    version="1.0.3",
     description="ChaCha20 encryption tools for Python.",
     url="https://github.com/thecodeforge/pychacha",
     py_modules=["pychacha"],
